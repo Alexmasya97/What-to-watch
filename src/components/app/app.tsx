23b2fb2a242e19bfb98cf8b69b9f2fae1deda91a
Alexmasya97 <sasa_97@mail.ru>
2025-03-06 19:07:24 +1000

    add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,16 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { Fragment } from 'react';
 import Main from '../main/main';
+import { AppRoute } from '../../const';
+import SignIn from '../../pages/Sign-in/Sign-in';
+import MyList from '../../pages/My-list/My-list';
+import MoviePage from '../../pages/Movie-page/Movie-page';
+import AdReview from '../../pages/Ad-review/Ad-review';
+import Player from '../../pages/Player/Player';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
+import { AuthorizationStatus } from '../../const';
+
 
 type AppProps = {
   name: string;
@@ -8,11 +20,66 @@ type AppProps = {
 
 function App({ name, genre, year }: AppProps): JSX.Element {
   return (
-    <Main
-      name={name}
-      genre={genre}
-      year={year}
-    />);
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main name={name} genre={genre} year={year} />}
+        />
+
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePage />}
+        />
+
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+
+        <Route path="*" element={
+          <Fragment>
+
+            <NotFound />
+            <Link to="/">Go to the main page</Link>
+          </Fragment>
+
+        }
+        />
+
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path={AppRoute.AddReview}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <AdReview />
+            </PrivateRoute>
+          }
+        />
+
+
+      </Routes>
+
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
